ff6113951371323bbc3483208dbfda34036ef702
scolli89 <35879707+scolli89@users.noreply.github.com>
2018-02-17 12:24:10 -0500

    RecursiveStringReplace
    Commiting RecursiveStringReplace.java
    after 6.3

---
+++ RecursiveStringReplace.java
@@ -0,0 +1,32 @@
+package recursion;
+public class RecursiveStringReplace
+{
+
+    /**
+     * replace - Replace all instances of one character by another.
+     * 
+     * @param  s   The string to do the replacement on.
+     * @param  from   The character to be replaced.
+     * @param  to   the character to change to.
+     * @return     A new string with the appropriate characters replaced.
+     */
+    public String replace(String s, char from, char to)
+    {
+        String result = null;
+
+        // IMPLEMENT THIS RECURSIVE METHOD
+//>>>>>>>>> ADDED CODE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+    if (s.length() < 1) {
+        return s;
+    }
+    else {
+        char one = (from == s.charAt(0)) ? to : s.charAt(0);
+        return one + replace(s.substring(1),from,to);
+}
+//<<<<<<<<<<<<<<<<<<<<<<<<<        
+
+    //return result;
+    }
+
+
+}
